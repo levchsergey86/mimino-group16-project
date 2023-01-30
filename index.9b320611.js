5b513f972ec7f3eb85b4bd8333da67b8e7cdc651
levchsergey86 <116516113+levchsergey86@users.noreply.github.com>
2023-01-30 17:51:52 +0000

    Deploying to gh-pages from @ levchsergey86/mimino-group16-project@fafe81b63663bfff7452264aca957ec95a18d033 🚀

---
+++ index.9b320611.js
@@ -0,0 +1,2 @@
+!function(){var o={openModalBtn:document.querySelector("[modal-open-room]"),closeModalBtn:document.querySelector("[modal-close-room]"),modal:document.querySelector("[modal-room]")};function e(){o.modal.classList.toggle("is-hidden")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)}();
+//# sourceMappingURL=index.9b320611.js.map
---
+++ restaurant.00efa37f.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn:document.querySelector("[modal-open-table]"),closeModalBtn:document.querySelector("[modal-close-table]"),modal:document.querySelector("[modal-table]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
+//# sourceMappingURL=restaurant.00efa37f.js.map
