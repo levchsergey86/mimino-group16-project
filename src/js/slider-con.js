117bd7eddbde866639696adf9231492373196db0
Maksym Yeroshevych <makserosh95@gmail.com>
2023-01-31 20:44:32 +0200

    add section details of our restaurant

--- src/js/slider-con.js
+++ src/js/slider-con.js
@@ -1,8 +1,9 @@
 function swiperSlider() {
-    const teamMain = document.querySelectorAll('[data-slider="team-slider"]');
+    const aboutRestMain = document.querySelectorAll('[data-slider="about-rest-slider"]');
+    const detRestMain = document.querySelectorAll('[data-slider="det-rest-slider"]');
     //const heroSlider = document.querySelectorAll('[data-slider="hero-slider"]');
-    if (teamMain) {
-        teamMain.forEach(slider => {
+    if (aboutRestMain) {
+        aboutRestMain.forEach(slider => {
             // кнопки вперед та назад
             let arrowLeft = slider.querySelector('.arrow-swiper-prev');
             let arrowRight = slider.querySelector('.arrow-swiper-next');
@@ -16,7 +17,7 @@ function swiperSlider() {
                 //     disableOnInteraction: false,
                 // },
                 slidesPerView: 2, // кількість слайдерів для показу
-                spaceBetween: 5, // відстань між слайдерами
+                spaceBetween: 25, // відстань між слайдерами
 
                 // кнопки навігації
                 navigation: {
@@ -64,6 +65,71 @@ function swiperSlider() {
             });
         });
     }
+    if (detRestMain) {
+        detRestMain.forEach(slider => {
+            // кнопки вперед та назад
+            let arrowLeft = slider.querySelector('.arrow-swiper-prev');
+            let arrowRight = slider.querySelector('.arrow-swiper-next');
+
+            let swiper = new Swiper(slider.querySelector('.swiper'), {
+                speed: 1500,
+                // автоплей
+                //centeredSlides: true,
+                // autoplay: {
+                //     delay: 3000,
+                //     disableOnInteraction: false,
+                // },
+                slidesPerView: 2, // кількість слайдерів для показу
+                spaceBetween: 0, // відстань між слайдерами
+
+                // кнопки навігації
+                navigation: {
+                    nextEl: arrowRight,
+                    prevEl: arrowLeft,
+                },
+
+                // додаємо додатковий клас
+                // можна використовувати для додаткових анімацій
+                on: {
+                    transitionStart: function () {
+                        let previousIndex = this.previousIndex;
+                        let previousSlide = slider.getElementsByClassName('swiper-slide')[previousIndex];
+                        if (previousSlide) {
+                            setTimeout(function () {
+                                previousSlide.classList.remove('is-play');
+                            }, 1000);
+                        }
+                    },
+                    transitionEnd: function () {
+                        let activeIndex = this.activeIndex;
+                        let activeSlide = slider.getElementsByClassName('swiper-slide')[activeIndex];
+                        activeSlide.classList.add('is-play');
+                    },
+                },
+                // адаптив
+                breakpoints: {
+                    // when window width is >= 320px
+                    414: {
+                        slidesPerView: 2,
+                        spaceBetween: 20,
+                    },
+                    // when window width is >= 768px
+                    1024: {
+                        slidesPerView: 3,
+                        spaceBetween: 34
+                    },
+                    // when window width is >= 1200px
+                    1280: {
+                        slidesPerView: 3,
+                        spaceBetween: 87
+                    }
+                }
+
+            });
+        });
+    }
 
 }
 window.addEventListener('load', swiperSlider, false);
+
+
