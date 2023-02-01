dcde5d483c04173dcd77f3c2920069ee6b9c7722
GlobalSatUA <alex.trenbach@gmail.com>
2023-02-01 17:43:37 +0200

    modals/ js correction

--- src/js/vendors/modal-room.js
+++ src/js/vendors/modal-room.js
@@ -1,28 +1,19 @@
 (() => {
-    const refs = {
-      openModalBtn: document.querySelector("[modal-open-room]"),
-      closeModalBtn: document.querySelector("[modal-close-room]"),
-      modal: document.querySelector("[modal-room]"),
-    };
-  
-    refs.openModalBtn.addEventListener("click", toggleModal);
-    refs.closeModalBtn.addEventListener("click", toggleModal);
-  
-    function toggleModal() {
-      refs.modal.classList.toggle("is-hidden");
-    }
-  })();
+  const refs = {
+    // openModalBtn: document.querySelector("[modal-open-price]"),
 
-  (() => {
-    const refs = {
-      openModalBtn: document.querySelector("[modal-open-room1]"),
-      modal: document.querySelector("[modal-room]"),
-    };
+    openModalBtn: document.querySelectorAll("[modal-open-room]").forEach(btn => { 
+      btn.addEventListener('click', toggleModal) 
+     }),
+
+    closeModalBtn: document.querySelector("[modal-close-room]"),
+    modal: document.querySelector("[modal-room]"),
+  };
   
-    refs.openModalBtn.addEventListener("click", toggleModal);
-    refs.closeModalBtn.addEventListener("click", toggleModal);
-  
-    function toggleModal() {
-      refs.modal.classList.toggle("is-hidden");
-    }
-  })();
+  // refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+  }
+})();
