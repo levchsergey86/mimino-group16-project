01c879086d43a0a5bfbf6c7c3efc823f84d992ff
GlobalSatUA <alex.trenbach@gmail.com>
2023-02-01 15:05:29 +0200

    modals-bug

--- src/js/vendors/modal-price.js
+++ src/js/vendors/modal-price.js
@@ -12,3 +12,17 @@
       refs.modal.classList.toggle("is-hidden");
     }
   })();
+
+  (() => {
+    const refs = {
+      openModalBtn: document.querySelector("[modal-open-price1]"),
+      modal: document.querySelector("[modal-price]"),
+    };
+  
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+  
+    function toggleModal() {
+      refs.modal.classList.toggle("is-hidden");
+    }
+  })();
