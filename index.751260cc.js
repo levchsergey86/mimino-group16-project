5f06ca50c7ae4c0027d639a3d5c4208b9932630e
levchsergey86 <116516113+levchsergey86@users.noreply.github.com>
2023-02-01 16:09:19 +0000

    Deploying to gh-pages from @ levchsergey86/mimino-group16-project@6568ce178dc3bb9aaa14a5e5fbceb5699a7ce808 🚀

---
+++ index.751260cc.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn:document.querySelectorAll("[modal-open-price]").forEach((function(e){e.addEventListener("click",o)})),closeModalBtn:document.querySelector("[modal-close-price]"),modal:document.querySelector("[modal-price]")};function o(){e.modal.classList.toggle("is-hidden")}e.closeModalBtn.addEventListener("click",o)}();
+//# sourceMappingURL=index.751260cc.js.map
