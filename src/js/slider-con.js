cfc2220eccdef82f7d330469bf52d922f01fe9d7
Maksym Yeroshevych <makserosh95@gmail.com>
2023-02-05 17:47:14 +0200

    fix bugs

--- src/js/slider-con.js
+++ src/js/slider-con.js
@@ -18,6 +18,7 @@ function swiperSlider() {
                 // },
                 slidesPerView: 2, // кількість слайдерів для показу
                 spaceBetween: 25, // відстань між слайдерами
+                loop: true,
 
                 // кнопки навігації
                 navigation: {
@@ -49,6 +50,7 @@ function swiperSlider() {
                     414: {
                         slidesPerView: 2,
                         spaceBetween: 25,
+                        
                     },
                     // when window width is >= 768px
                     1024: {
@@ -81,6 +83,7 @@ function swiperSlider() {
                 // },
                 slidesPerView: 2, // кількість слайдерів для показу
                 spaceBetween: 0, // відстань між слайдерами
+                loop: true,
 
                 // кнопки навігації
                 navigation: {
@@ -122,6 +125,7 @@ function swiperSlider() {
                     1280: {
                         slidesPerView: 3,
                         spaceBetween: 87
+                        
                     }
                 }
 
